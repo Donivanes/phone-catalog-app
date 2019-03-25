8997a75e090a66f27443533327dfac6e9a9a411f
Giorgio Grassini <giorgiograssini@gmail.com>
2019-03-25 20:34:32 +0100

    Added redux ✨

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,25 @@
 /* eslint-disable no-undef */
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import App from './App';
+import rootReducer from './reducers';
 import './sass/main.scss';
 
-const App = () => (
-  <div>
-    <h1>Hello Parcel</h1>
-  </div>
+/* eslint-disable no-underscore-dangle */
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const middleware = [thunk];
+const store = createStore(
+  rootReducer,
+  composeEnhancer(applyMiddleware(...middleware)),
 );
 
-render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app'),
+);
