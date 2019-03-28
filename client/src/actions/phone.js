3c047fb440839427f11738735d2754eb6f72ea40
Giorgio Grassini <giorgiograssini@gmail.com>
2019-03-29 00:32:04 +0100

    Added tests in action 🧪

--- client/src/actions/phone.js
+++ client/src/actions/phone.js
@@ -5,10 +5,11 @@ import { FETCH_DATA_FULFILLED, FETCH_DATA_REJECTED } from '../constants/ActionTy
 // eslint-disable-next-line import/prefer-default-export
 export const fetchData = () => (dispatch, getState) => {
   const { phone } = getState();
-  if (phone.data == null) {
+  if (!phone || phone.data === null) {
     const getPhones = 'http://localhost:3000/api/phones';
     return axios.get(getPhones)
       .then((response) => {
+        console.log(response)
         dispatch({ type: FETCH_DATA_FULFILLED, payload: response.data });
       })
       .catch((err) => {
